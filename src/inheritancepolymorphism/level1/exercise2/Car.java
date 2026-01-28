a1b858b3cfc81121343dae6e55101b125f6fbafe
nilbm <n.blasmor@gmail.com>
2026-01-28 13:00:04 +0100

    feat: add Car class

---
+++ src/inheritancepolymorphism/level1/exercise2/Car.java
@@ -0,0 +1,14 @@
+package inheritancepolymorphism.level1.exercise2;
+
+public class Car {
+
+    private static final String brand = "Volvo";
+    private static String model = "XYZ123";
+    private final int power;
+
+    public Car(int power) {
+        this.power = power;
+    }
+
+}
+
