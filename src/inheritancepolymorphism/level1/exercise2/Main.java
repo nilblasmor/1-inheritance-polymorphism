8171497f51deb1f538f42a0b700c8f0b1c81bb1e
nilbm <n.blasmor@gmail.com>
2026-01-28 13:06:57 +0100

    feat: demonstrate brake() and accelerate() in Main

---
+++ src/inheritancepolymorphism/level1/exercise2/Main.java
@@ -0,0 +1,13 @@
+package inheritancepolymorphism.level1.exercise2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Car.brake();
+
+        Car c1 = new Car(200);
+
+        c1.accelerate();
+
+    }
+}
